57f45bebb8d062f41578dbadcc5836a844763145
yallrick <vincentdelvalle@gmail.com>
2015-03-20 16:26:51 +0100

    création du premir controller API

---
+++ src/RogerTruckSupervisor/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace RogerTruckSupervisor\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+
+    /**
+     * @Route("/jfeejzfjezjfo")
+     * @Method("POST")
+     * @Template()
+     */
+    public function sendAction()
+    {
+
+        $response = new JsonResponse();
+        $array_documents = array();
+
+        // TODO
+
+        $response->setContent(json_encode( $array_documents ));
+        return $response;
+    }
+
+
+    /**
+     * @Route("/demo")
+     * @Method("GET")
+     * @Template()
+     */
+    public function demoAction()
+    {
+
+        $response = new JsonResponse();
+        $array_documents = array();
+
+        // TODO
+        
+        $array_documents[] = array("code" => 200, "message" => "touvabien");
+        $array_documents[] = array("code" => 200, "message" => "touvabien");
+        $array_documents[] = array("code" => 200, "message" => "touvabien");
+        $array_documents[] = array("code" => 200, "message" => "touvabien");
+
+        $response->setContent(json_encode( $array_documents ));
+        return $response;
+    }
+}
---
+++ src/RogerTruckSupervisor/AppBundle/RogerTruckSupervisorAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RogerTruckSupervisor\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RogerTruckSupervisorAppBundle extends Bundle
+{
+}
