7d8e5bdf6006724ac8e7e977dbb6927832b73821
yallrick <vincentdelvalle@gmail.com>
2015-03-27 16:09:02 +0100

    méthode d'enregistrement de la position des camions vroumvroum

--- src/RogerTruckSupervisor/AppBundle/Controller/DefaultController.php
+++ src/RogerTruckSupervisor/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -25,11 +26,11 @@ class DefaultController extends Controller
         return array('name' => $name);
     }
     
-    private function logIntoParseFacebook($whattodo){
+    private function logIntoParseFacebook($whattodo, array $options = null){
         ParseClient::initialize('oL1kxNThX8882iThZhowKQgGMtcX9u93fMYZzRhc', 'OPMvbyuFsR91hkftjr98w80GRpu1HuY4j9DGI7pC', '2zWN6c32DFiFrY4zch0yhLI7dHXHefluRYf8O1ZV');
         try{
             $user = ParseUser::logIn("simon", "simon");
-            return $whattodo($user);
+            return $whattodo($user, $options);
         }catch(ParseException $error){
 
         }
@@ -67,6 +68,23 @@ class DefaultController extends Controller
         return $this->logIntoParseFacebook($whatareyougonnadonow);
 
     }
+
+    /**
+     * @Route(  "/truck/{id_truck}/location")
+     * @Method("POST")
+     */
+    public function truckUpdateLocationAction($id_truck){
+        
+        $foo = function($user, $options){
+            $idTruck = $options['id_truck'];
+            
+            return new Response($idTruck);
+        };
+        return $this->logIntoParseFacebook($foo, array('id_truck' => $id_truck));
+        
+    }
+    
+    
 
 
     /**
