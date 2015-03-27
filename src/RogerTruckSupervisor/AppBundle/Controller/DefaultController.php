6226d2da838068dcd37c7931c0cb5c1a3681569a
Simon Ansquer <simon.ansquer@y-nov.com>
2015-03-27 16:34:52 +0100

    test

--- src/RogerTruckSupervisor/AppBundle/Controller/DefaultController.php
+++ src/RogerTruckSupervisor/AppBundle/Controller/DefaultController.php
@@ -88,27 +88,28 @@ class DefaultController extends Controller
 
 
     /**
-     * @Route("/truck/{id}")
+     * @Route("/truck/{id_truck}")
      * @Method("GET")
      * @Template()
      */
-    public function getTruckInfo($id)
+    public function truckInfoAction($id_truck)
     {
-        $response = new JsonResponse();
-        $array_documents = array();
-    
-        // TODO
-        $query = new ParseQuery("Camion");
-        try {
-          $gameScore = $query->get($id);
-          // The object was retrieved successfully.
-        } catch (ParseException $ex) {
-          // The object was not retrieved successfully.
-          // error is a ParseException with an error code and message.
-        }
-        $model = $gameScore->get("model");
-        $response->setContent(json_encode( $model ));
-      
-        return new Response();
+        $yolo = function($user, $options){
+            $idTruck = $options['id_truck'];
+
+
+            $response = new JsonResponse();
+            $array_documents = array();
+            $query = new ParseQuery("Camion");
+
+            $result = $query->get($idTruck);
+            echo "Successfully retrieved " . count($result) . " camion.";
+
+            $array_documents[] = $result->get("model");
+            $response->setContent(json_encode( $array_documents ));
+            return $response;
+            
+        };
+        return $this->logIntoParseFacebook($yolo, array('id_truck' => $id_truck));
     }
 }
